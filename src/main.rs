9921c953fdd24b161fa9b68450d81ab5beea74f4
Victor Kostyuk <torcoste@gmail.com>
2023-07-16 01:03:53 +0200

    CLI draft implemented

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Spend {},
+    Update {
+        #[arg(long)]
+        force: bool,
+    },
+}
+
 fn main() {
-    println!("Hello, sweet!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Commands::Spend {}) => {
+            println!("Spend");
+        }
+        Some(Commands::Update { force }) => {
+            println!("Update. force={}", &force);
+        }
+        None => {
+            println!("no command was provided");
+        }
+    }
 }
